9adb6f4336b89e7a10e2cc3ddf08389dca72e0e2
Daniel Romero <danielromero1213@hotmail.com>
2020-12-16 23:37:24 -0500

    creamos la division

--- calculadora.js
+++ calculadora.js
@@ -3,11 +3,13 @@ var $num2 = document.getElementById("num2");
 var $suma = document.getElementById("suma");
 var $resta = document.getElementById("resta");
 var $multiplicacion = document.getElementById("multiplicacion");
+var $division = document.getElementById("division");
 var $resultado = document.getElementById("resultado");
 
 $suma.addEventListener("click", sumar);
 $resta.addEventListener("click", restar);
 $multiplicacion.addEventListener("click", multiplicar);
+$division.addEventListener("click", dividir);
 
 function sumar(e) {
     e.preventDefault();
@@ -22,6 +24,12 @@ function restar(e) {
 }
 
 function multiplicar(e) {
+    e.preventDefault();
+    var resultado = parseInt($num1.value) + parseInt($num2.value);
+    $resultado.innerText = resultado;
+}
+
+function dividir(e) {
     e.preventDefault();
     var resultado = parseInt($num1.value) + parseInt($num2.value);
     $resultado.innerText = resultado;
